979d5c38b1d087ca85f3925cccf23c75b57e1383
Leon Lu <ltlu1993@gmail.com>
2022-09-20 17:23:59 -0400

    Springboot generated

---
+++ Project/Backend/src/main/java/Self/demo/WeaponRouletteApplication.java
@@ -0,0 +1,13 @@
+package Self.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeaponRouletteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeaponRouletteApplication.class, args);
+	}
+
+}
---
+++ Project/Backend/src/test/java/Self/demo/WeaponRouletteApplicationTests.java
@@ -0,0 +1,13 @@
+package Self.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeaponRouletteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
